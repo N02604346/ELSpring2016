7fd2924961fd149bc403fbcadc306de1292f1cc2
N02604346 <N02604346@hawkmail.newpaltz.edu>
2016-02-25 21:17:13 +0000

    Fixed syntax errors! was previously unable to use Pi.

--- code/myBlinkingLed.py
+++ code/myBlinkingLed.py
@@ -6,14 +6,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)	
 
 def Blink():
-	while True
-		for i in range(0,3)
+	while True:
+		for i in range(0,3):
 			GPIO.output(17,True)
 			time.sleep(.2)
 			GPIO.output(17,False)
 			time.sleep(.2)
 		time.sleep(5)
-		for i in range(0,4)
+		for i in range(0,4):
 			GPIO.output(17,True)
 			time.sleep(.2)
 			GPIO.output(17,False)

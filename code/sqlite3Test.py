72cb7fccfbd80393a6df5f1f744daf53cf2d454a
N02604346 <N02604346@hawkmail.newpaltz.edu>
2016-03-07 02:14:36 +0000

    fixed python program to properly log date and time. fixed datetime usage and execute statement. also added commit and close statements.

--- code/sqlite3Test.py
+++ code/sqlite3Test.py
@@ -9,22 +9,30 @@ import sqlite3 as mydb
 import sys
 import datetime
 
-con = None
+def LogTime():
 
-try:
-	con = mydb.connect('DateTime.db')
-	cur = con.cursor()
-	t = datetime.time
-	d = datetime.date
-	xdate = str(d.year) + '-' + str(d.month) + '-' + str(d.day)
-	xtime = str(t.hour) + '-' + str(t.minute) + '-' + str(t.second)
-	cur.execute("INSERT INTO DTA (DATE, TIME) VALUES (" + xdate + ", " + xtime");")
+        con = None
 
-except mydb.Error, e:
+        try:
+                con = mydb.connect('DateTime.db')
+                cur = con.cursor()
+                t = datetime.datetime.time(datetime.datetime.now())
+                d = datetime.date.today()
+                xdate = str(d)
+                xtime = str(t)
+                cur.execute("INSERT INTO DTA VALUES (?, ?)", (xdate, xtime))
+                con.commit()
+                con.close()
+                print xtime
+                print xdate
+                print "logged"
+
+        except mydb.Error, e:
 	
-	print "Error %s:" % e.args[0]
+                print "Error %s:" % e.args[0]
 
-finally:
+        finally:
 
-	if con:
-		con.close()
+                if con:
+                        con.close()
+LogTime()
